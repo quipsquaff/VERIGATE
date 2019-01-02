f455eb76755e7d38de4cf81b751dc04c2155b5ee
Tristan Perera <tristan.e.perera@gmail.com>
2019-01-02 15:23:00 -0800

    added routes for sign-out and login, added middleware to config

--- routes/register.js
+++ routes/register.js
@@ -1,10 +1,33 @@
 var express = require('express');
 var router  = express.Router();
 
-var register_controller = require('../controllers/register_controller');
+var passport = require("../config/passport");
+var users_controller = require('../controllers/users_controller');
+var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 router.get('/', register_controller.index);
+
+router.get('/sign-out', register_controller.signOutUser);
+
+router.post('/login', passport.authenticate("local"), register_controller.loginUser);
 
 router.post('/register', register_controller.create);
 
 module.exports = router;
+
+
+
+
+// var express = require('express');
+// var router  = express.Router();
+
+// var register_controller = require('../controllers/register_controller');
+
+// router.get('/', register_controller.index);
+
+// router.post('/register', register_controller.create);
+
+// module.exports = router;
+
+
+
