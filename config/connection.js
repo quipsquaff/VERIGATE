9090e76573f62a87a98bfcce16838f87b5d155ca
quipsquaff <quipsquaff@gmail.com>
2019-01-03 19:55:26 -0800

    pass

--- config/connection.js
+++ config/connection.js
@@ -5,15 +5,8 @@ var connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "pol1cestate",
-    database: "verigate_db"
-  });
 }
+
 // else {
 //   connection = mysql.createConnection({
 //     host: "mgs0iaapcj3p9srz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
