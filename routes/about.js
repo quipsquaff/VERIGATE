4ce47792a9bfa5266427cc9522a1609dd233d1db
Tristan Perera <tristan.e.perera@gmail.com>
2019-01-04 16:31:35 -0800

    about page routed correctly

--- routes/about.js
+++ routes/about.js
@@ -1,11 +1,10 @@
 var express = require("express");
 var router = express.Router();
 
-// var application_controller = require("../controllers/about");
-// var devices_controller = require("../controllers/devices_controller");
+var about_controller = require("../controllers/about_controller");
 // var isAuthenticated = require("../config/middleware/isAuthenticated");
 
-// router.get('/about', isAuthenticated, application_controller.index);
+router.get('/', about_controller.index);
 
 // router.put('/about', isAuthenticated, application_controller.updateSwitch); // Route to UPDATE switchState on the 'gates' table.
 
